4cb09441775877b98654776091a63a04b1419cca
Natatashkin <cs070283sng@gmail.com>
2022-06-12 00:56:09 +0300

    fix getAllContacts, delete extra console.logs

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -1,13 +1,35 @@
 const { Contact } = require("../../models");
+// const { NotFound } = require("http-errors");
 
 const getAllContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10, favorite = false } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id, favorite }, "", {
+
+  if (favorite === "true") {
+    const contacts = await Contact.find({ owner: _id, favorite }, "", {
+      skip: skip,
+      limit: Number(limit),
+    }).populate("owner", " _id name email subscription");
+
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        result: contacts,
+      },
+    });
+  }
+
+  // if (favorite === "false") {
+  //   throw new NotFound("лісом!");
+  // }
+
+  const contacts = await Contact.find({ owner: _id }, "", {
     skip: skip,
     limit: Number(limit),
   }).populate("owner", " _id name email subscription");
+
   res.json({
     status: "success",
     code: 200,
